bb05fff959c2539565a3ed9a1f666e35477def33
edlingao <edlingao@edlingao.com>
2023-12-24 04:03:50 -0600

    fixing cors

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,7 @@
 use std::{sync::{Mutex, Arc}, collections::HashSet};
 
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, http::{Method, header::CONTENT_TYPE, HeaderValue}};
+use tower_http::cors::{CorsLayer, Any};
 
 use crate::types::{State, SharedState};
 
@@ -13,10 +14,14 @@ pub fn router() -> Router {
     }));
 
     let router = Router::new();
-    
+    //let origins = [
+     //   "localhost:5173".parse().unwrap(),
+       // "https://www.restaurant.edlingao.me".parse().unwrap(),
+    //];
     router
         .route("/", get(|| async { "Hello, World!" }))
         .route("/restaurants/:location", get(restaurant::endpoints::restaurants))
         .with_state(Arc::clone(&state))
+        .layer(CorsLayer::permissive())
 }
 
